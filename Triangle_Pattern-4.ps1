77f99af365430a9384f08b98fc1eee54201215b9
agakhil13 <aguptaji1310@gmail.com>
2023-04-09 11:03:32 +0530

    Triangle Patterns - Downward

---
+++ Triangle_Pattern-4.ps1
@@ -0,0 +1,9 @@
+﻿for ($i = 0; $i -le 5; $i++){
+    for ($sp = 0; $sp -lt $i; $sp++) {
+        Write-Host " " -NoNewline
+    }
+    for ($j = 5; $j -ge $i; $j-- ){
+        Write-Host " *" -NoNewline
+    }
+    Write-Host
+}
