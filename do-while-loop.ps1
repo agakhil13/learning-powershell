b23e1eb8a45c19e8f1e6c74520843c7ff6ca056a
agakhil13 <aguptaji1310@gmail.com>
2023-04-09 11:30:31 +0530

    do while and print table

---
+++ do-while-loop.ps1
@@ -0,0 +1,6 @@
+﻿$Names = @("Bob", "Alice", "Joy", "Anne", "Demma")
+$counter = $Names.Length - 1
+Do {
+    Write-Host "Hello,"$Names[$counter]
+    $counter--
+} while ($counter -ge 0)
---
+++ print-a-table.ps1
@@ -0,0 +1,11 @@
+﻿$Number = Read-Host "Enter a number to print a table: "
+$Number = [int]$Number
+
+for ($i = 1; $i -le 10; $i++){
+    if ($i%2){
+        Write-Host $Number X $i = ($Number*$i) -BackgroundColor DarkYellow
+    }
+    else {
+        Write-Host $Number X $i = ($Number*$i) -BackgroundColor Red
+        }
+}
