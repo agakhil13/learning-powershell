6f741bded706df1fc04eb21f8fce3302ed9eb41c
agakhil13 <aguptaji1310@gmail.com>
2023-04-09 10:59:59 +0530

    Triangle Patterns

---
+++ Triangle_Pattern-1.ps1
@@ -0,0 +1,6 @@
+﻿for ($i = 0; $i -le 5; $i++){
+    for ($j = 0; $j -le $i; $j++) {
+        Write-Host "* " -NoNewline
+    } 
+    Write-Host
+}
---
+++ Triangle_Pattern-3.ps1
@@ -0,0 +1,9 @@
+﻿for ($i = 0; $i -le 5; $i++){
+    for ($sp = 5; $sp -gt $i; $sp--) {
+        Write-Host " " -NoNewline
+    }
+    for ($j = 0; $j -le $i; $j++ ){
+        Write-Host " *" -NoNewline
+    }
+    Write-Host
+}
